239f17b7299ef2eed8869614005ef0ee6468e25f
Mike Miroliubov <mmiroliubov@ggasoftware.com>
2015-09-22 17:05:05 +0300

    Loading and showing all variants

--- src/main/webapp/js/vcf.js
+++ src/main/webapp/js/vcf.js
@@ -5,62 +5,126 @@
 
 $(function() {
     var vcfH = 100;
-    var W = $("body").width();
+    var zoom = 1;
+    var W = $("body").width() * zoom;
+    var minWidth = 0.3;
+    var padding = 2;
+    var vcfData;
     var svgVcf = d3.select("div#variation_plot").append("svg").attr("width", W).attr("height", vcfH);
 
-    $.ajax({
-        url: "/gene-browser/services/variants",
-        method: "POST",
-        contentType: "application/json",
-        data: JSON.stringify({
-            chrId: "A1",
-            from: 2772000,
-            to: 2774500
-        }),
-        success: function (data) {
-            console.log(data);
-            var xScale = d3.scale.linear()
-                .domain([
-                    d3.min(data, function (d) {
-                        return d.position
-                    }),
-                    d3.max(data, function (d) {
-                        return d.position
-                    })
-                ])
-                .range([0, W]);
+    function position(data) {
+        return data.position;
+    }
 
-            var groups = svgVcf.selectAll("rect").data(data).enter().append("g");
-            groups.append("rect").attr({
-                y: 0,
+    function loadVariations(chrId, from, to) {
+        $.ajax({
+            url: "/gene-browser/services/variants",
+            method: "POST",
+            contentType: "application/json",
+            data: JSON.stringify({
+                chrId: chrId,
+                from: from,
+                to: to
+            }),
+            success: function (data) {
+                console.log(data);
+                vcfData = data;
+
+                drawVcf(data);
+            }
+        });
+    }
+
+    function drawVcf(data) {
+        var xScale = d3.scale.linear()
+            .domain([
+                d3.min(data, position),
+                d3.max(data, position)
+            ])
+            .range([0, W - padding]);
+
+        var groups = svgVcf.selectAll("g").data(data, position);
+
+        // who enter
+        var g = groups.enter().append("g");
+        var width = W / (d3.max(data, function (d) {
+                return d.position
+            }) - d3.min(data, function (d) {
+                return d.position
+            }));
+
+        g.append("rect").attr({
+            class:"ref",
+            y: 0,
+            x: function (data, i) {
+                return xScale(data.position);
+            },
+            height: vcfH / 2,
+            width: width > minWidth ? width : minWidth,
+            fill: "blue"
+        });
+
+        g.append("rect")
+            .attr({
+                class:"alt",
+                y: vcfH / 2,
                 x: function (data, i) {
                     return xScale(data.position);
                 },
-                height: vcfH / 2,
-                width: W / (d3.max(data, function (d) {
-                    return d.position
-                }) - d3.min(data, function (d) {
-                    return d.position
-                })),
-                fill: "blue"
+                height: function (data) {
+                    return vcfH / 2;
+                },
+                width: width > minWidth ? width : minWidth,
+                fill: "red"
             });
 
-            groups.append("rect")
-                .attr({
-                    y: vcfH / 2,
-                    x: function (data, i) {
-                        return xScale(data.position);
-                    },
-                    height: function (data) {
-                        return vcfH / 2;
-                    },
-                    width: W / (d3.max(data, function (d) {
-                        return d.position
-                    }) - d3.min(data, function (d) {
-                        return d.position
-                    })),
-                    fill: "red"
-                });
-        }
+        // who update
+        g = groups;
+        g.selectAll("rect.ref").attr({
+            class:"ref",
+            y: 0,
+            x: function (data, i) {
+                return xScale(data.position);
+            },
+            height: vcfH / 2,
+            width: width > minWidth ? width : minWidth,
+            fill: "blue"
+        });
+
+        g.selectAll("rect.alt").attr({
+            class:"alt",
+            y: vcfH / 2,
+            x: function (data, i) {
+                return xScale(data.position);
+            },
+            height: function (data) {
+                return vcfH / 2;
+            },
+            width: width > minWidth ? width : minWidth,
+            fill: "red"
+        });
+
+        //who remove
+        groups.exit().remove();
+    }
+
+    loadVariations("A1", 29186, 239107476);
+
+    $("button#search").on("click", function(e) {
+        e.preventDefault();
+        var chrId = $('#chrId').val();
+        var from = $('#from').val();
+        var to = $('#to').val();
+
+        loadVariations(chrId, from, to);
     });
+
+    $("button#zoom_in").on("click", function(e) {
+        e.preventDefault();
+        zoom++;
+        W = $("body").width() * zoom;
+        $("#zoom").val(zoom);
+        d3.select("div#variation_plot").select("svg").attr("width", W);
+        drawVcf(vcfData);
+    })
 });
